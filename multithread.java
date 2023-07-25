a84521db1f457b583ddf11c5127401f04e951921
21WH1A1204 - Anjani Sukanya <109974831+AnjaniSukanya@users.noreply.github.com>
2023-07-25 11:34:07 +0530

    Create multithread.java

---
+++ multithread.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.Random;
+class Square extends Thread 
+{
+	int x;
+	Square(int n)
+	{
+		 x = n;
+	}
+
+	public void run()
+	{
+		int sqr = x * x;
+		System.out.println("Square of " + x + " = " + sqr);
+	}
+
+}
+
+
+class Cube extends Thread
+{
+	int x;
+	Cube(int n)
+	{
+		 x = n;
+	}
+
+	public void run()
+	{
+		int cub = x*x*x;
+		System.out.println("Cube of " + x + " = " + cub);
+	}
+}
+
+
+class multithread
+{
+	public static void main(String[] args)
+        {
+          
+			Random rand = new Random();
+			for(int i=0;i<10;i++)
+			{
+				int randint = rand.nextInt(100);
+				System.out.println("Random int : " +randint);
+				if(randint % 2 == 0)
+				{
+					Square s = new Square(randint);
+					s.start();
+				}
+				else
+				{
+					Cube c = new Cube(randint);
+					c.start();
+				}
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch( InterruptedException ex)
+				{
+					System.out.println(ex);
+				}
+			}
+	}
+}
+		
+
